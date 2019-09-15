931c188b35dd7e76befc5c48461b1f9d9a8f9a59
Kelsey <kelzling@gmail.com>
2019-09-15 12:44:14 +1200

    Implemented Loader and the basics of SudokuManager that were required for it

---
+++ Loader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku
+{
+    static class Loader
+    {
+        static SudokuManager LoadLevel(string fileName)
+        {
+            using (StreamReader reader = new StreamReader(fileName))
+            {
+                string fileContents = reader.ReadToEnd();
+                return new SudokuManager(fileContents);
+            }
+        }
+    }
+}
---
+++ SudokuManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku
+{
+    class SudokuManager : ISet, IGet
+    {
+        protected Grid myGrid;
+        protected int[] levelTemplate;
+
+        public SudokuManager()
+        {
+            // just for not crashing things purposes right now
+            myGrid = new Grid();
+            levelTemplate = new int[0];
+        }
+
+        public SudokuManager(string gridContentsCSV)
+        {
+            myGrid = new Grid();
+            myGrid.FromCSV(gridContentsCSV);
+            levelTemplate = myGrid.ToArray();
+        }
+
+        public int GetByColumn(int columnIndex, int rowIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetByRow(int rowIndex, int columnIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetBySquare(int squareIndex, int positionIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetByColumn(int value, int columnIndex, int rowIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetByRow(int value, int rowIndex, int columnIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetBySquare(int value, int squareIndex, int positionIndex)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
