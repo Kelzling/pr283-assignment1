7aaa52647699181e1dce3816c8c3aac2dc2a1de9
Chris Wordsworth <wordsworth.soundz@live.com>
2019-09-18 16:14:51 +1200

    Fixed conflicts

--- Tests/TestIntegration.cs
+++ Tests/TestIntegration.cs
@@ -16,14 +16,9 @@ namespace Sudoku.Tests
         [SetUp]
         protected void SetUp()
         {
-<<<<<<< HEAD
-            goodSudoku = Loader.LoadLevel(@"C:\Users\words\OneDrive - Ara Institute of Canterbury\BICT Private Folders\Semester 4\PR283 dotNET\Projects\cs293-assignment1\Levels\test1.txt");
-            badSudoku = Loader.LoadLevel(@"C:\Users\words\OneDrive - Ara Institute of Canterbury\BICT Private Folders\Semester 4\PR283 dotNET\Projects\cs293-assignment1\Levels\test2.txt");
-=======
             string basePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Levels");
             goodSudoku = Loader.LoadLevel($@"{basePath}\test1.txt");
             badSudoku = Loader.LoadLevel($@"{basePath}\test2.txt");
->>>>>>> 0bd35f1323c470e61e389a45a214ef69d2315ab0
         }
 
         // test the Row(index).IsValid() etc combinations
